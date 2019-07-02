625b42a83ff6c34fb28e7c64267d4fea2707b397
pokemonwei <pokemonwei@126.com>
2019-07-02 22:44:56 +0800

    finish grpc-install.sh,start writing test unixopen

---
+++ os_remote/test/test_client.c
@@ -0,0 +1,46 @@
+
+#include "sqlite3.c"
+
+#include <stdio.h>
+
+int main()
+{
+    #define UNIXVFS(VFSNAME, FINDER) {                        \
+    3,                    /* iVersion */                    \
+    sizeof(unixFile),     /* szOsFile */                    \
+    MAX_PATHNAME,         /* mxPathname */                  \
+    0,                    /* pNext */                       \
+    VFSNAME,              /* zName */                       \
+    (void*)&FINDER,       /* pAppData */                    \
+    unixOpen,             /* xOpen */                       \
+    unixDelete,           /* xDelete */                     \
+    unixAccess,           /* xAccess */                     \
+    unixFullPathname,     /* xFullPathname */               \
+    unixDlOpen,           /* xDlOpen */                     \
+    unixDlError,          /* xDlError */                    \
+    unixDlSym,            /* xDlSym */                      \
+    unixDlClose,          /* xDlClose */                    \
+    unixRandomness,       /* xRandomness */                 \
+    unixSleep,            /* xSleep */                      \
+    unixCurrentTime,      /* xCurrentTime */                \
+    unixGetLastError,     /* xGetLastError */               \
+    unixCurrentTimeInt64, /* xCurrentTimeInt64 */           \
+    unixSetSystemCall,    /* xSetSystemCall */              \
+    unixGetSystemCall,    /* xGetSystemCall */              \
+    unixNextSystemCall,   /* xNextSystemCall */             \
+  }
+
+  /*
+  ** All default VFSes for unix are contained in the following array.
+  **
+  ** Note that the sqlite3_vfs.pNext field of the VFS object is modified
+  ** by the SQLite core when the VFS is registered.  So the following
+  ** array cannot be const.
+  */
+  static sqlite3_vfs aVfs[] = {
+    UNIXVFS("unix",          posixIoFinder ),
+  };
+  
+
+    return 0;
+}
--- tool/grpc_install.sh
+++ tool/grpc_install.sh
@@ -1,2 +1,45 @@
 # ! /bin/bash
 
+#sudo yum install gflags-devel gtest-devel
+#sudo yum install libcxx-devel
+
+git clone -b $(curl -L https://grpc.io/release) https://github.com/grpc/grpc
+cd grpc
+git submodule update --init
+
+cd ./third_party/protobuf/
+ ./configure --prefix=$HOME/software/protobuf-install
+make clean 
+make
+make install
+
+cd ../../
+
+#grpc
+sed "s|^prefix ?=.*|prefix ?= $HOME/software/grpc-install|" -i ./Makefile
+make clean
+make 
+make install
+
+
+echo 'export PROTOBUF="$HOME/software/protobuf-install"' >> ~/.bashrc
+
+echo 'export GRPC="$HOME/software/grpc-install"' >> ~/.bashrc
+
+echo 'export PATH="$PROTOBUF/bin:$GRPC/bin:$PATH"' >> ~/.bashrc
+
+echo 'export LD_LIBRARY_PATH="$PROTOBUF/lib:$GRPC/lib:$LD_LIBRARY_PATH"' >> ~/.bashrc
+
+echo 'export PKG_CONFIG_PATH="$GRPC/lib/pkgconfig:$PROTOBUF/lib/pkgconfig:$PKG_CONFIG_PATH"' >> ~/.bashrc
+
+echo 'export C_INCLUDE_PATH="$GRPC/include/:$C_INCLUDE_PATH"' >> ~/.bashrc
+
+echo 'export CPLUS_INCLUDE_PATH="$GRPC/include/:$CPLUS_INCLUDE_PATH"' >> ~/.bashrc
+
+echo 'export LIBRARY_PATH="$PROTOBUF/lib:$GRPC/lib:$LIBRARY_PATH"' >> ~/.bashrc
+
+
+exec "$SHELL"
+
+
+
